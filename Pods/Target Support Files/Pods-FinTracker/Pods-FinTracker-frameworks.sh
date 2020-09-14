ee0bcf0ff989e753e01d0c186a35bbca75b39340
Виталий <its.vint@ya.ru>
2020-09-14 13:35:12 +0300

    Минорное обновление Realm

--- Pods/Target Support Files/Pods-FinTracker/Pods-FinTracker-frameworks.sh
+++ Pods/Target Support Files/Pods-FinTracker/Pods-FinTracker-frameworks.sh
@@ -198,11 +198,9 @@ fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
